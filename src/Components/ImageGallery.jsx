efe6dcdb3678b2d4946d025c934947ffc24e02c2
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2022-02-20 12:23:50 -0800

    css cleanup + refactor

--- src/Components/ImageGallery.jsx
+++ src/Components/ImageGallery.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const ImageGallery = styled.img`
-  margin: 0.625rem 0.625rem 0 0;
+  margin: 0.625rem 0.3rem 0 0.3rem;
   width: 18.75rem;
   max-width: 100%;
   background-color: #fff;
