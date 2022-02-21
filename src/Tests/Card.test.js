62f16fd3634f73fab212479f6d4d47a693c883e2
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2022-02-20 16:43:26 -0800

    Adding tests

---
+++ src/Tests/Card.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import Card from '../Components/Card'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<Card />, div)
+})
+
+it('renders correctly', () => {
+  const tree = renderer.create(<Card />)
+  expect(tree).toMatchSnapshot()
+})
