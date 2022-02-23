bc92765970e3b2da9fcacb171aea707c6cffbac5
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2022-02-22 21:41:43 -0800

    loader timing

--- src/Components/LoaderIcon.jsx
+++ src/Components/LoaderIcon.jsx
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 import { rotate } from './keyFrames.style'
 
 const LoadingIcon = styled.img`
-  width: 3rem;
   margin: 0 auto;
+  width: 3rem;
   animation: ${rotate} 4s linear infinite;
 `
 export default LoadingIcon
