03fcceef45516ef31f6558a0006d439c4f454673
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2022-02-22 17:03:31 -0800

    appdata update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import React, { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AppContainer from './Components/AppContainer'
 import Loader from './Components/Loader'
 import CardOne from './Components/CardOne'
 import CardTwo from './Components/CardTwo'
 import Footer from './Components/Footer'
-import appData from './AppData/AppData'
+import { appData as data } from './AppData/AppData'
 
 const App = () => {
+  const [appData, setAppData] = useState(null)
+  const [isloading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const getAppData = () => {
+      try {
+        const response = data
+        setAppData(response)
+        setError(null)
+      } catch (err) {
+        setError(err.message)
+        console.error(error)
+        setAppData(null)
+      } finally {
+        setLoading(false)
+      }
+    }
+    getAppData()
+  }, [])
+
   return (
     <>
-      {!appData.length ? (
+      {isloading ? (
         <AppContainer>
           <Loader />
         </AppContainer>
