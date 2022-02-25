97932f35ef010eff77ec1b45cd751c8c1d61061f
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2022-02-25 11:08:51 -0800

    removed unused node packages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './Components/App'
-import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,7 +9,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 )
-
-reportWebVitals()
 
 history.scrollRestoration = 'manual'
